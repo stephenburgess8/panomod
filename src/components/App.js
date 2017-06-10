9fccf971fdc8b4bfa701ae88585014f9210af7d7
Stephen Burgess <stephenburgess8@gmail.com>
2017-06-10 15:58:02 -0500

    add image

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Interactive from 'react-interactive';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
-import ExampleComponent from './ExampleComponent';
+import PanoViewer from './PanoViewer';
 import PageNotFound from './PageNotFound';
 import Breadcrumbs from './Breadcrumbs';
 import s from '../styles/app.style';
@@ -24,7 +24,7 @@ export default function App() {
 
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/example" component={ExampleComponent} />
+        <Route path="/panoviewer" component={PanoViewer} />
         <Route component={PageNotFound} />
       </Switch>
 
---
+++ src/components/PanoViewer.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Interactive from 'react-interactive';
+import { Switch, Route, Link } from 'react-router-dom';
+import ExampleTwoDeepComponent from './ExampleTwoDeepComponent';
+import PageNotFound from './PageNotFound';
+import s from '../styles/panoViewer.style';
+
+const ExamplePageText = () => (
+  <p style={s.p}>
+    This is an example page. Refresh the page or copy/paste the url to
+    test out the redirect functionality (this same page should load
+      after the redirect).
+  </p>
+);
+
+const PanoView = () => (
+  <div className="panoview">
+    <img className="panoview-image" src="resources/images/panorama_arboretum_1.jpg" alt="Arboretum Panorama" />
+  </div>
+);
+
+export default function PanoViewer() {
+  return (
+    <Switch>
+      <Route
+        exact path="/panoviewer/two-deep"
+        render={({ location }) => (
+          <div>
+            <ExamplePageText />
+            <ExampleTwoDeepComponent location={location} />
+          </div>
+        )}
+      />
+      <Route
+        exact path="/example"
+        render={() => (
+          <div>
+            <PanoView />
+            <div style={s.pageLinkContainer}>
+              <Interactive
+                as={Link}
+                {...s.link}
+                to="/panoviewer/two-deep?field1=foo&field2=bar#boom!"
+              >Example two deep with query and hash</Interactive>
+            </div>
+          </div>
+        )}
+      />
+      <Route component={PageNotFound} />
+    </Switch>
+  );
+}
